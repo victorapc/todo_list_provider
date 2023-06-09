a078af747b051598f737cb7abf2e6b05a26a8cff
victor <victor.alexandre@atlantasistemas.com>
2023-06-09 15:54:32 -0300

    Configuração FireBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/app_module.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const AppModule());
 }
