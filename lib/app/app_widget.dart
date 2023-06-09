5250f62cacca3293c7ee2e07f57cb7bafd6c6023
victor <victor.alexandre@atlantasistemas.com>
2023-06-09 14:08:56 -0300

    Pagina de cadastro e google service

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
 import 'package:todo_list_provider/app/core/ui/todo_list_ui.config.dart';
 import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
-import 'package:todo_list_provider/app/modules/auth/login/login_controller.dart';
-import 'package:todo_list_provider/app/modules/auth/login/login_page.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
 
 class AppWidget extends StatefulWidget {
@@ -20,12 +17,12 @@ class _AppWidgetState extends State<AppWidget> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance?.addObserver(sqliteAdmConnection);
+    WidgetsBinding.instance.addObserver(sqliteAdmConnection);
   }
 
   @override
   void dispose() {
-    WidgetsBinding.instance?.removeObserver(sqliteAdmConnection);
+    WidgetsBinding.instance.removeObserver(sqliteAdmConnection);
     super.dispose();
   }
 
