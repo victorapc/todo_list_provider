682fb755368c4053cd0acb63c77c9c6aead51eac
victor <victor.alexandre@atlantasistemas.com>
2023-06-12 15:02:08 -0300

    Repository e service do User

---
+++ lib/app/repositories/user/user_repository_impl.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_list_provider/app/exception/auth_exception.dart';
+import 'package:todo_list_provider/app/repositories/user/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  FirebaseAuth _firebaseAuth;
+
+  UserRepositoryImpl({required FirebaseAuth firebaseAuth})
+      : _firebaseAuth = firebaseAuth;
+
+  @override
+  Future<User?> register(String email, String password) async {
+    try {
+      final userCredencial = await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+
+      return userCredencial.user;
+    } on FirebaseAuthException catch (e, s) {
+      print(e);
+      print(s);
+      if (e.code == 'email-already-in-use') {
+        final loginTypes =
+            await _firebaseAuth.fetchSignInMethodsForEmail(email);
+        if (loginTypes.contains('password')) {
+          throw AuthException(
+              message: 'E-mail já utilizado, por favor escolha outro e-mail.');
+        } else {
+          throw AuthException(
+              message:
+                  'Você se cadastrou no ToDoList pelo Google, por favor utilize ele para entrar.');
+        }
+      } else {
+        throw AuthException(message: e.message ?? 'Erro ao registrar usuário.');
+      }
+    }
+  }
+}
